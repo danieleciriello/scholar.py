696efca22a5bc8c8ffaac5bbe1534235d0773ee6
Danele Ciriello <ciriello.daniele@gmail.com>
2015-09-27 13:16:18 +0200

    re-add exceptions

--- src/scholar.py
+++ src/scholar.py
@@ -47,6 +47,17 @@ else:
             return str(s)
 
 
+class Error(Exception):
+    """Base class for any Scholar error."""
+
+
+class FormatError(Error):
+    """A query argument or setting was formatted incorrectly."""
+
+
+class QueryArgumentError(Error):
+    """A query did not have a suitable set of arguments."""
+
 class ScholarConf(object):
 
     """Helper class for global settings."""
